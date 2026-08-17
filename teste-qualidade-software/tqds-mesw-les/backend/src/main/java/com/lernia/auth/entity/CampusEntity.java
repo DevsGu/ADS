3b5bb3f2be9d67117d594ac81bff9b15d0dd64a3
unknown <studyguuhss@gmail.com>
2026-08-16 21:41:57 -0300

    :recycle: Refactor: Refatorando CampusEntity

--- teste-qualidade-software/tqds-mesw-les/backend/src/main/java/com/lernia/auth/entity/CampusEntity.java
+++ teste-qualidade-software/tqds-mesw-les/backend/src/main/java/com/lernia/auth/entity/CampusEntity.java
@@ -1,20 +1,28 @@
 package com.lernia.auth.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "campuses", schema = "lernia")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class CampusEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "university_id", nullable = false)
     private UniversityEntity university;
+
+    @Column(nullable = false)
     private String name;
+
     private String description;
     private String country;
     private String city;
